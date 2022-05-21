72e56571d964f542435b5a59ad8a8c7d6d980402
Shadat Hossien <sahadatsays@gmail.com>
2022-05-21 14:27:17 +0600

    User crud Done

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,10 +1,16 @@
 import express from "express"
 import { verifyToken } from "../../utils/verifyToken.js";
+import { list, update, show, destroy } from "../../controllers/UserController.js";
 
 const router = express.Router();
-verifyToken
+
 router.get('/check-auth', verifyToken, (req, res, next) => {
      res.send('auth');
 })
+
+router.get("/", verifyToken, list);
+router.get('/:id', verifyToken, show)
+router.put('/:id', verifyToken, update)
+router.delete('/:id', verifyToken, destroy)
 
 export default router;
